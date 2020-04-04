b3d7e60732e1ac883b3c66e12ae285e8d73ad0c2
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-04-04 20:19:44 +0530

    Create Conditional Statements.c

---
+++ Conditional Statements.c
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string a[]={"Greater than 9","one","two","three","four","five","six","seven","eight","nine"};
+    int n;
+    cin>>n;
+    cout<<((n>9)?a[0]:a[n]);
+    return 0;
+}
+
+/*
+Input (stdin)
+  5
+Expected Output
+  five
+*/
