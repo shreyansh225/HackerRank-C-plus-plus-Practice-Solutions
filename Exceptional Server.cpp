5e32869d3c5bc647262c02a18d150d737d9f6c57
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-04-29 20:44:33 +0530

    Create Exceptional Server.cpp

---
+++ Exceptional Server.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <exception>
+#include <string>
+#include <stdexcept>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+class Server {
+private:
+	static int load;
+public:
+	static int compute(long long A, long long B) {
+		load += 1;
+		if(A < 0) {
+			throw std::invalid_argument("A is negative");
+		}
+		vector<int> v(A, 0);
+		int real = -1, cmplx = sqrt(-1);
+		if(B == 0) throw 0;
+		real = (A/B)*real;
+		int ans = v.at(B);
+		return real + A - B*ans;
+	}
+	static int getLoad() {
+		return load;
+	}
+};
+int Server::load = 0;
+
+int main() {
+	int T; cin >> T;
+	while(T--) {
+		long long A, B;
+		cin >> A >> B;
+
+		try {
+            cout << Server::compute(A,B) << endl;
+        } 
+        catch (bad_alloc& error) {
+            cout << "Not enough memory" << endl;
+        }
+        catch (exception& error) {
+            cout << "Exception: " << error.what() << endl;
+        }
+        catch (...) {
+            cout << "Other Exception" << endl;
+        }
+	}
+	cout << Server::getLoad() << endl;
+	return 0;
+}
+
+/*
+If the compute function runs fine with the given arguments, then print the result of the function call.
+If it fails to allocate the memory that it needs, print Not enough memory.
+If any other standard C++ exception occurs, print Exception: S where S is the exception's error message.
+If any non-standard exception occurs, print Other Exception.
+
+Input (stdin)
+
+2
+-8 5
+1435434255433 5
+
+Output
+Exception: A is negative
+Not enough memory
+2
+*/
